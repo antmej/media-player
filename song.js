126125258c43ea6af966a1f83512dbdeef183bcd
Antonio Mejia <mejiaaj21@gmail.com>
2015-11-09 20:20:34 -0500

    Added artist, duration and album to media player

--- song.js
+++ song.js
@@ -1,94 +1,79 @@
 
-function Song(title, artist, duration, path) { // parameters/arguments being passed in by line 3 in app.js
+function Song(title, artist, duration, album, path) { // parameters/arguments being passed in by line 3 in app.js
 	Media.call(this, title, duration); // Media object handles title and duration
 	this.artist = artist; // Song object is handles artist
+	this.album = album;
 	this.path = path; // path to audio file
 }
 
 Song.prototype = Object.create(Media.prototype);
 // copies the references to Media's prototype properties and methods to Song's prototype properties
 
-// LOOK UP .CALL METHOD AND PROTOTYPE INHERITANCE
+Song.prototype.toHTML = function () {
+	var track, artist, duration, album, addToPlaylist;
 
-Song.prototype.toHTML = function() {
-	var htmlString = '<li';
+
+	track = '<li';
 	if (this.isPlaying) { // if true had a class of "current" to 
-		htmlString += ' class="current"';
+		track += ' class="current"';
 	}
-	htmlString += '>';
-	htmlString += this.title;
-	htmlString += ' - ';
-	htmlString += this.artist;
-	htmlString += '<span class="duration">';
-	htmlString += this.duration;
-	htmlString += '</span></li>';
-	// console.log(htmlString);
-	return htmlString;
-	// <li class="current"> Midnight City - M83 <span class="duration"> 4:04 </span></li>
+	track += '>'
+	track += this.title;
+	track += '</li>'
+
+	artist = '<li'
+	if (this.isPlaying) { // if true had a class of "current" to 
+		artist += ' class="current"';
+	}
+	artist += '>'
+	artist += this.artist;
+	artist += '</li>'
+
+	duration = '<li'
+	if (this.isPlaying) { // if true had a class of "current" to 
+		duration += ' class="current"';
+	}
+	duration += '>'
+	duration += this.duration;
+	duration += '</li>'
+
+	album = '<li'
+	if (this.isPlaying) { // if true had a class of "current" to 
+		album += ' class="current"';
+	}
+	album += '>'
+	album += this.album;
+	album += '</li>'
+
+	return addToPlaylist = track + artist + duration + album;
+
+
+	// var htmlString = '<li';
+	// if (this.isPlaying) { // if true had a class of "current" to 
+	// 	htmlString += ' class="current"';
+	// }
+	// htmlString += '>';
+	// htmlString += this.title;
+	// htmlString += ' - ';
+	// htmlString += this.artist;
+	// htmlString += '<span class="duration">';
+	// htmlString += this.duration;
+	// htmlString += '</span></li>';
+	// // console.log(htmlString);
+	// return htmlString;
+	// // <li class="current"> Midnight City - M83 <span class="duration"> 4:04 </span></li>
 };
 
-Song.prototype.audioSource = function() {
-	var audio = "<audio> <source src='midnight-city.mp3' type='audio/mpeg'> </audio>";
+Song.prototype.audioPlay = function () {
+	var audio = document.getElementById('audio');
+	audio.src = this.path;
+	audio.play(); // .play() method on audio element is built in method
 	return audio;
 };
 
-
-/*
-
-var m83 = {
-	title: "Midnight City",
-	artist: "M83",
-	duration: "4:04",
-	isPlaying: false,
-	play: function () {
-		this.isPlaying = true;
-	},
-	stop: function () {
-		this.isPlaying = false;
-	},
-	toHTML: function() {
-		var htmlString = '<li';
-		if(this.isPlaying) {
-			htmlString += ' class="current"';
-		}
-		htmlString += '>';
-		htmlString += this.title;
-		htmlString += ' - ';
-		htmlString += this.artist;
-		htmlString += '<span class="duration">';
-		htmlString += this.duration;
-		htmlString += '</span></li>';
-		// console.log(htmlString);
-		return htmlString;
-	}
-}
-
-var sweetDreams = {
-	title: "Sweet Dreams",
-	artist: "Beyonce",
-	duration: "3:28",
-	isPlaying: false,
-	play: function () {
-		this.isPlaying = true;
-	},
-	stop: function () {
-		this.isPlaying = false;
-	},
-	toHTML: function() {
-		var htmlString = '<li';
-		if(this.isPlaying) {
-			htmlString += ' class="current"';
-		}
-		htmlString += '>';
-		htmlString += this.title;
-		htmlString += ' - ';
-		htmlString += this.artist;
-		htmlString += '<span class="duration">';
-		htmlString += this.duration;
-		htmlString += '</span></li>';
-		// console.log(htmlString);
-		return htmlString;
-	}
-}
-
-*/
+Song.prototype.audioStop = function () {
+    var audio = document.getElementById('audio');
+    audio.pause(); // .pause() method on audio element is built in method
+    audio.src = ''; // removing the source is a trick to stop the audio instead of just pausing it
+    return audio;
+};
