16d11655bc98bc8511da8ef35029f564ebea776f
Antonio Mejia <mejiaaj21@gmail.com>
2016-09-09 01:38:00 -0400

    Code refactor

--- song.js
+++ song.js
@@ -13,60 +13,26 @@ Song.prototype = {
 
 	toHTML: function(trackNumber, index) {
 
-		var i, 
-		num, 
-		artist, 
-		duration, 
-		album, 
-		track, 
-		moreInfo, 
-		addToPlaylist;
-
-		i = parseInt(index);	
-		num = "<li>" + "<span class='tracknumber'>" + trackNumber + "</span>" + this.title + "</li>";
-		artist = "<li>" + this.artist + "</li>";
-		duration = "<li>" + this.duration + "</li>";
-		album = "<li>" + this.album + "</li>";
-
-		track = "<ul class=\'track\'";
-		track += "data-index=\'" + index + "\'";
-		track += "onclick=\'selectTrack(this.getAttribute(\"data-index\"));\'";
-		track += "ondblclick=\'doubleClickTrack(this.getAttribute(\"data-index\"));\'>";
-		track += "<li><i class=\'fa fa-play circle\' data-index=\'" + index + "\' onclick=\'playIcon(this.getAttribute(\"data-index\"));\'></i></li>";
-		track += num + artist + duration + album;
-		track += "<li><i class=\'info fa fa-caret-down square\' data-index=\'" + index + "\' onclick=\'runToggle(this.getAttribute(\"data-index\"));\'></i></li>";
-		track += "</ul>"
+		var i = parseInt(index);
+		var index = index;
+		var trackNumber = trackNumber;
+		
+		var title = this.title;
+		var	artist = this.artist;
+		var duration = this.duration;
+		var album = this.album; 
 
 
-		if (this.isPlaying === true) { 
-			track = "<ul class=\'track select\'"; 
-			track += "data-index=\'" + index + "\'";
-			track += "onclick=\'selectTrack(this.getAttribute(\"data-index\"));\'";
-			track += "ondblclick=\'doubleClickTrack(this.getAttribute(\"data-index\"));\'>";
-			track += "<li><i class=\'fa fa-volume-up circle\'";
-			track += "data-index=\'" + index + "\'";
-			track += "onclick=\'stopIcon();\'></i></li>";
-			track += num + artist + duration + album;
-			track += "<li><i class=\'info fa fa-caret-down square\' data-index=\'" + index + "\' onclick=\'runToggle(this.getAttribute(\"data-index\"));\'></i></li>";
-			track += "</ul>"
-		} // if track audio is playing change highlight track and show play icon
-			// <ul class='track select'>
-			// <i class='fa fa-volume-up circle'>
 
-		moreInfo = "<ul id=\'toggle" + index + "\'>";
-		moreInfo += "<li>" + "<img src=\'" + playlist.artist[i].img + "\'" + " alt=\'artist album cover\'" + ">" + "</li>";
-		moreInfo += "<li>" + "<h1>" + playlist.artist[i].name + "</h1>";
-		moreInfo += "<p>" + playlist.artist[i].bio + "<a href=\'" + playlist.artist[i].link + "\'";
-		moreInfo += " target=\'_blank\'>(more)" + "</a>" + "</p>" + "</li>";
-		moreInfo += "<li>" + "<h3>Albums</h3>";
-		moreInfo += "<span>" + playlist.artist[i].album1 + "</span>" + "<hr>" + "<br>";
-		moreInfo += "<span>" + playlist.artist[i].album2 + "</span>" + "<hr>" + "<br>" + "</li>";
-		moreInfo += "</ul>";
+		if (this.isPlaying === true) { 	
+			// updates HTML to show which track is playing
+			return isPlaying(index, trackNumber, title, artist, duration, album, i);
+		}
 
 
-		// *create fragment and then return addToPlaylist
+		// builds HTML for each track
+		return buildTrack(index, trackNumber, title, artist, duration, album, i); 
 
-		return addToPlaylist = track + moreInfo;
 	},
 
 	nowPlaying: function () {
---
+++ track.js
@@ -0,0 +1,64 @@
+
+function buildTrack(index, trackNumber, title, artist, duration, album, i) {
+
+		var track = "<ul class=\'track\'";
+			track += "data-index=\'" + index + "\'";
+			track += "onclick=\'selectTrack(this.getAttribute(\"data-index\"));\'";
+			track += "ondblclick=\'doubleClickTrack(this.getAttribute(\"data-index\"));\'>";
+			track += "<li><i class=\'fa fa-play circle\' data-index=\'" + index + "\' onclick=\'playIcon(this.getAttribute(\"data-index\"));\'></i></li>";
+			track += "<li><span class='tracknumber'>" + trackNumber + "</span>" + title + "</li>";
+			track += "<li>" + artist + "</li>";
+			track += "<li>" + duration + "</li>";
+			track += "<li>" + album + "</li>";
+			track += "<li><i class=\'info fa fa-caret-down square\' data-index=\'" + index + "\' onclick=\'runToggle(this.getAttribute(\"data-index\"));\'></i></li>";
+			track += "</ul>"
+
+		return track + songInfo(i, index);
+
+}
+
+
+function isPlaying(index, trackNumber, title, artist, duration, album, i) {
+
+		var track = "<ul class=\'track select\'";
+			track += "data-index=\'" + index + "\'";
+			track += "onclick=\'selectTrack(this.getAttribute(\"data-index\"));\'";
+			track += "ondblclick=\'doubleClickTrack(this.getAttribute(\"data-index\"));\'>";
+			track += "<li><i class=\'fa fa-volume-up circle\' data-index=\'" + index + "\' onclick=\'stopIcon(this.getAttribute(\"data-index\"));\'></i></li>";
+			track += "<li><span class='tracknumber'>" + trackNumber + "</span>" + title + "</li>";
+			track += "<li>" + artist + "</li>";
+			track += "<li>" + duration + "</li>";
+			track += "<li>" + album + "</li>";
+			track += "<li><i class=\'info fa fa-caret-down square\' data-index=\'" + index + "\' onclick=\'runToggle(this.getAttribute(\"data-index\"));\'></i></li>";
+			track += "</ul>"
+
+		return track + songInfo(i, index);
+		// updates 
+		// <ul class='track select'>
+		// <i class='fa fa-volume-up circle'>
+		// stopIcon()
+}
+
+
+function songInfo(i, index) {
+
+	var info = "<ul id=\'toggle" + index + "\'>";
+		info += "<li>" + "<img src=\'" + playlist.artist[i].img + "\'" + " alt=\'artist album cover\'" + ">" + "</li>";
+		info += "<li>" + "<h1>" + playlist.artist[i].name + "</h1>";
+		info += "<p>" + playlist.artist[i].bio + "<a href=\'" + playlist.artist[i].link + "\'";
+		info += " target=\'_blank\'>(more)" + "</a>" + "</p>" + "</li>";
+		info += "<li>" + "<h3>Albums</h3>";
+		info += "<span>" + playlist.artist[i].album1 + "</span>" + "<hr>" + "<br>";
+		info += "<span>" + playlist.artist[i].album2 + "</span>" + "<hr>" + "<br>" + "</li>";
+		info += "</ul>";
+
+	return info;
+
+}
+
+
+
+
+
+
+
